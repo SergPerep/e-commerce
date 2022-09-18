82c62c61c67f73a8266fcabf09f852789c6062df
Sergei Perepelkin <perepelkin.serge@gmail.com>
2022-09-18 11:38:17 +0200

    Center image of a product

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,7 +2,7 @@ const Product = ({ src, title }) => {
   return (
     <div className="product">
       <div className="product__image-wrapper">
-        <img className="product__image" src={src} alt="" />
+        <img src={src} alt="" />
       </div>
       <span className="product__title">{title}</span>
     </div>
